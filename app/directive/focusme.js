849f130aa7736adc8fb568eefdb89e3bcb0f2828
Scott Lee <seungyol72@gmail.com>
2016-11-11 21:51:15 +1000

    Clean code

--- app/directive/focusme.js
+++ app/directive/focusme.js
@@ -4,7 +4,6 @@ angular.module('myApp')
     link: function(scope, element, attrs) {
       var model = $parse(attrs.focusMe);
       scope.$watch(model, function(value) {
-//        console.log('value=',value);
         if(value === true) { 
           $timeout(function() {
             
@@ -16,7 +15,6 @@ angular.module('myApp')
         }
       });
       element.bind('blur', function() {
-//        console.log('blur')
         scope.$apply(model.assign(scope, false));
       })
     }
